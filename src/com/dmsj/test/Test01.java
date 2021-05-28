46ab8da412e7d74ab2f2fe50769f4bdcb333086e
一念轮回 <84893920+cycleInMind@users.noreply.github.com>
2021-05-28 16:06:10 +0800

    测试 ，pull

--- src/com/dmsj/test/Test01.java
+++ src/com/dmsj/test/Test01.java
@@ -9,5 +9,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("测试idea使用github");
         System.out.println("测试idea使用github，push");
+        System.out.println("测试idea使用github，pull");
     }
 }
