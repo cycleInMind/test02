d57c7762ee86f1bf954fde7158e8f624823eaee2
luyu <2715980436@qq.com>
2021-05-28 15:50:57 +0800

    Initial commit

---
+++ src/com/dmsj/test/Test01.java
@@ -0,0 +1,12 @@
+package com.dmsj.test;
+
+/**
+ * @Description: TODO
+ * @author: scott
+ * @date: 2021年05月28日 15:39
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("测试idea使用github");
+    }
+}
